f08bc771cbf97269cdfbb20a47490bc61bb0f0bf
khb532 <khb532@naver.com>
2022-09-23 20:05:15 +0900

    0923commit_none

---
+++ webdev2022/src/test/java/com/example/webdev2022/Webdev2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdev2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webdev2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
