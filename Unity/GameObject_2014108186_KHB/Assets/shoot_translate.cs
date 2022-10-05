6541c87674c2f6ce84fb7a97b4b63d99342bf1ce
khb532 <khb532@naver.com>
2022-10-05 17:50:47 +0900

    22.10.05 commit // Unity

---
+++ Unity/GameObject_2014108186_KHB/Assets/shoot_translate.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot_translate : MonoBehaviour
+{
+    float speed = 5.0f;
+    float power = 500.0f;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance_per_frame = speed * Time.deltaTime;
+        float vertical_input = Input.GetAxis("Vertical");
+        float horizontal_input = Input.GetAxis("horizontal");
+        Vector3 launch_direction = new Vector3(0,1,1);
+
+        transform.Translate(Vector3.forward * vertical_input * distance_per_frame);
+        transform.Translate(Vector3.right * horizontal_input * distance_per_frame);
+
+        if(Input.GetButtonDown("Fire1"))
+            ball.GetComponent<Rigidbody>().AddForce(launch_direction * power);
+    }
+}
