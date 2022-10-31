fc1b938ff9ed98ecf773239111825302278c1105
khb532 <khb532@naver.com>
2022-10-31 23:11:54 +0900

    22.10.31 commit // CPP

--- CPP/Chapter11/DeleteOverloading.cpp
+++ CPP/Chapter11/DeleteOverloading.cpp
@@ -11,13 +11,13 @@ public:
 	Point(int x=0, int y=0):xpos(x),ypos(y){}
 	friend ostream& operator<<(ostream& os, const Point& pos);
 
-	void* operator new(size_t size)
+	void* operator new(size_t size)	// new연산자 오버로딩, 반환형은 반드시 void* , 매개변수는 size_t
 	{
 		cout << "operator new : " << size << endl;
 		void* adr = new char[size];
 		return adr;
 	}
-	void operator delete(void* adr)
+	void operator delete(void* adr)	// delete연산자 오버로딩, 반환형 void, 매개변수 void*
 	{
 		cout << "operator deletd()" << endl;
 		delete[] adr;
