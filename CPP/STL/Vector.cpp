74abcdb2245540913e7b69edf3b9efcc31ae35d7
khb532 <khb532@naver.com>
2022-11-24 16:47:45 +0900

    22.11.24 cpp

--- CPP/STL/Vector.cpp
+++ CPP/STL/Vector.cpp
@@ -144,3 +144,49 @@ int main(void)
 
 	return 0;
 }
+
+
+/*
+#include <vector>                    // vector가 들어있는 헤더파일
+vector<int> v;                       // int타입 벡터 생성
+vector<int> v = { 1, 2, 3};          // int형 백터 생성 후 1, 2, 3 으로 초기화
+vector<int> v[10];                   // int타입 벡터 배열(크기 : 10) 생성
+vector<int> v[] = {{ 1, 2}, {3, 4}}; // int형 백터 배열 생성(행은 가변이지만 열은 고정)
+vector<vector<int>> v;               // 2차원 백터 생성(행과 열 모두 가변)
+vector<int> v(5);                    // 5개의 원소를 0으로 초기화
+vector<int> v(5, 3);                 // 5개의 원소를 3으로 초기화
+vector<int> v2(v);                   // 벡터 v를 복사하여 벡터v2 생성
+*/
+/*
+v.push_back(10);  // 마지막 위치에 숫자 10 추가
+
+vector<int>::iterator it = v.begin();
+it = v.insert(it, 2); //맨앞에 2를 삽입
+it = v.insert(it, 2, 3); // 맨앞에 3을 2개 삽입
+it = v.insert(it+2, 2, 4); // 2번째부터 4를 2개 삽입
+*/
+
+/*
+v.pop_back();                        // 마지막에 넣은 값 제거
+v.erase(vec.begin()+10);             // index 10의 값을 제거
+v.erase(vec.begin(), vec.begin()+5);  // index 0~5의 값을 제거
+v.clear();                           //모든 값 제거
+*/
+
+/*
+v.size();   //vector의 원소 갯수
+v.capacity; //vector의 물리적 크기
+
+vector<int> v;
+for (int i=0; i<5; i++)
+  v.push_back(i); //vector 요소 추가
+
+
+for (int i = 0; i < v.size(); i++)
+  cout << v[i] << " "; //모든 값 출력 : 0 1 2 3 4
+
+cout << v[2] << endl;      //index 2의 값 출력 : 2
+cout << v.front() << endl; //처음 요소 출력 : 0
+cout << v.back() << endl;  //마지막 요소 출력 : 4
+*/
+
