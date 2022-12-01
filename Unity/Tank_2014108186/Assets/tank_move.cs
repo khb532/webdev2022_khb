e1dce7a1ea24df284a6a3c768bcb8ad9f7744dac
khb532 <khb532@naver.com>
2022-12-01 14:46:04 +0900

    22.12.01 unity

---
+++ Unity/Tank_2014108186/Assets/tank_move.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tank_move : MonoBehaviour
+{
+    private float tank_speed = 5.0f;
+    private float rot_speed = 120.0f;
+    public GameObject turret;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance_per_frame = tank_speed * Time.deltaTime;
+        float degrees_per_frame = rot_speed * Time.deltaTime;
+
+        float moving_velocity = Input.GetAxis("Vertical");
+        float tank_angle = Input.GetAxis("Horizontal");
+        float turret_angle = Input.GetAxis("TurretRotation");
+
+        this.transform.Translate(Vector3.forward * moving_velocity * distance_per_frame);
+        this.transform.Rotate(0.0f, tank_angle * degrees_per_frame, 0.0f);
+        turret.transform.Rotate(Vector3.up * turret_angle * degrees_per_frame * 0.5f);
+    }
+}
