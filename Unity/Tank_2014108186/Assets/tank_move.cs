e5e0a6a39c99bd76502df0b5fe33cc4dce54cfa0
khb532 <khb532@naver.com>
2022-12-02 13:07:38 +0900

    11.12.02  cpp+unity

--- Unity/Tank_2014108186/Assets/tank_move.cs
+++ Unity/Tank_2014108186/Assets/tank_move.cs
@@ -6,7 +6,10 @@ public class tank_move : MonoBehaviour
 {
     private float tank_speed = 5.0f;
     private float rot_speed = 120.0f;
+    public float bullet_power = 1000.0f;
     public GameObject turret;
+    public Transform bullet;
+    public GameObject barrel;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +29,13 @@ public class tank_move : MonoBehaviour
         this.transform.Translate(Vector3.forward * moving_velocity * distance_per_frame);
         this.transform.Rotate(0.0f, tank_angle * degrees_per_frame, 0.0f);
         turret.transform.Rotate(Vector3.up * turret_angle * degrees_per_frame * 0.5f);
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject spawn_point = GameObject.Find("sp_bullet");
+            Transform prefab_bullet = Instantiate(bullet, spawn_point.transform.position, spawn_point.transform.rotation);
+            prefab_bullet.GetComponent<Rigidbody>().AddForce(turret.transform.forward * bullet_power);
+        }
     }
 }
+
